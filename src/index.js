2ee504717b4f9bb66eee5a1f8b8d21f7fc6a17cc
Sam O'Cain <samocain93@gmail.com>
2023-06-10 22:11:46 -0500

    added grayscale class for sold out item

--- src/index.js
+++ src/index.js
@@ -118,15 +118,16 @@ function Menu() {
 function Pizza({ pizzaObj }) {
   console.log(pizzaObj);
 
-  if (pizzaObj.soldOut) return null;
+  //   if (pizzaObj.soldOut) return null;
 
   return (
-    <li className='pizza'>
+    <li className={`pizza ${pizzaObj.soldOut ? 'sold-out' : ""}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.alt}></img>
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <p>{pizzaObj.price}</p>
+
+        <p>{pizzaObj.soldOut ? 'SOLD OUT' : pizzaObj.price}</p>
       </div>
     </li>
   );
